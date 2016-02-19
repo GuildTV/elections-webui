a9d5e6399f2182bea69d8c2f85d2012de445a865
Julian Waller <git@julusian.co.uk>
2016-02-19 23:38:46 +0000

    More templates on candidates

--- js/components/Dashboard.js
+++ js/components/Dashboard.js
@@ -3,9 +3,11 @@
 */
 
 import React from 'react';
+import $ from 'jquery';
 import {
   Grid, Row, Col, 
-  Input, Button
+  Input, Button,
+  Popover, OverlayTrigger
 } from 'react-bootstrap';
 import Socket from 'react-socket';
 
@@ -13,6 +15,9 @@ import Socket from 'react-socket';
 * Variables
 */
 const GetPeopleKey = "getPeople";
+const RunTemplateKey = "runTemplate";
+const SetWinnerKey = "setWinner";
+const ClearWinnerKey = "clearWinner";
 
 /*
 * React
@@ -49,7 +54,10 @@ export default class Dashboard extends React.Component {
   render() {
     var peopleList = this.state.people
       .filter((p) => Dashboard.filterPerson(this.state.filter, p))
-      .map((p) => <PersonEntry key={p.id} data={p} />);
+      .map((p) => <PersonEntry key={p.id} refs={this.refs} parent={this} data={p} />);
+
+    $('.popover').remove();
+
 
     return (
       <div>
@@ -94,14 +102,58 @@ export default class Dashboard extends React.Component {
   }
 }
 
+const overlayCss = {
+  marginTop: "72px",
+  textAlign: "center"
+}
+
 class PersonEntry extends React.Component {
+  runTemplate(e){
+    console.log("Running template:", e.target.getAttribute('data-id'));
+
+    this.props.refs.sock.socket.emit(RunTemplateKey, {
+      template: e.target.getAttribute('data-id'),
+      data: this.props.data
+    });
+  }
+
+  setWinner(e){
+    console.log("Setting winner:", this.props.data.id);
+
+    this.props.refs.sock.socket.emit(SetWinnerKey, this.props.data);
+
+    //TODO - update client
+  }
+
+  cleareWinner(e){
+    console.log("Clearing winner:", this.props.data.id);
+
+    this.props.refs.sock.socket.emit(ClearWinnerKey, this.props.data);
+
+    //TODO - update client
+  }
+
   render() {
+    var overlayContent = (
+      <Popover title="More Templates" style={overlayCss}>
+        <Button data-id="SidebarPhoto" onClick={this.runTemplate.bind(this)}>Sidebar - Photo</Button>
+        <Button data-id="sidebarText" onClick={this.runTemplate.bind(this)}>Sidebar - Text</Button>
+        {
+          this.props.data.elect ? 
+            <Button onClick={this.clearWinner.bind(this)}>Clear Winner</Button> : 
+            <Button onClick={this.setWinner.bind(this)}>Mark Winner</Button>
+        }
+      </Popover>
+    );
+
     return (
       <Col md={4} sm={6} xs={12} style={{ textAlign: "center" }}>
         <p>{ this.props.data.firstName } { this.props.data.lastName }</p>
         <p>
-          <Button>Lower Third</Button>
-          <Button>More</Button>
+          <Button data-id="lowerThird" onClick={this.runTemplate.bind(this)}>Lower Third</Button>&nbsp;
+          <OverlayTrigger container={this.props.parent} trigger="click" placement="right" overlay={ overlayContent }>
+            <Button bsStyle="info">More</Button>
+          </OverlayTrigger>
         </p>
       </Col>
     );
